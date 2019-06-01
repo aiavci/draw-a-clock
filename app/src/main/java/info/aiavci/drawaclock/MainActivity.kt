f2d605786a43346941f642e17f844ee0ba85ed16
Ali Avci <aiavci@yahoo.com>
2019-06-01 17:18:06 -0400

    Integrate OpenCV

--- app/src/main/java/info/aiavci/drawaclock/MainActivity.kt
+++ app/src/main/java/info/aiavci/drawaclock/MainActivity.kt
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
 
     var resultFragment = ResultFragment()
